dfe04bb9976dc6f69db311f838cb8acd321db4d3
Jun Nakajima <jun.nakajima@uzabase.com>
2021-10-17 18:16:31 +0900

    json assert と e2e プロジェクト追加

---
+++ json-assert/src/main/java/Sample.kt
@@ -0,0 +1,5 @@
+
+
+class Sample {
+    fun hoge() = "hoge"
+}
---
+++ json-assert/src/test/java/SampleTest.kt
@@ -0,0 +1,11 @@
+
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+internal class HogeTest {
+    @Test
+    fun hoge() {
+        assertTrue(true)
+    }
+}
