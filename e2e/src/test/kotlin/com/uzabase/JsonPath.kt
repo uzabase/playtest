e695dc1f05df8ec7fcf4e20244bbc9f97aa80e91
jnuank <ikuta1919@gmail.com>
2021-10-23 13:21:38 +0900

    e2e: jsonPathが文字列のvalueであることをアサートができる Pass

--- e2e/src/test/kotlin/com/uzabase/JsonPath.kt
+++ e2e/src/test/kotlin/com/uzabase/JsonPath.kt
@@ -5,12 +5,12 @@ import org.amshove.kluent.`should not throw`
 import org.amshove.kluent.fail
 import org.amshove.kluent.invoking
 
-class JsonPath {
+class JsonPath: JsonAssert {
     @Step("<jsonPath>が文字列の<value>であることをアサー卜できる")
     fun assertJsonPathString(jsonPath: String, value: String) {
         val json = """"
             {"key1": {"key2": "test"}}
             """".trimMargin()
-        invoking { fail("not implemented") } `should not throw` AssertionError::class
+        invoking { json.assertByJsonPath(value, jsonPath) } `should not throw` AssertionError::class
     }
 }
