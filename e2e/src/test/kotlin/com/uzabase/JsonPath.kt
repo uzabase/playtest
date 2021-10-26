6292ea12496fd8a20cb10fe5440d74308ef504cf
jnuank <ikuta1919@gmail.com>
2021-10-27 08:35:07 +0900

    jsonPathで値を取得するE2Eテストのみ作成

--- e2e/src/test/kotlin/com/uzabase/JsonPath.kt
+++ e2e/src/test/kotlin/com/uzabase/JsonPath.kt
@@ -5,35 +5,27 @@ import org.amshove.kluent.`should not throw`
 import org.amshove.kluent.invoking
 
 class JsonPath : JsonAssert {
-    @Step("<jsonPath>が文字列の<value>であることをアサー卜できる")
-    fun assertJsonPathString(jsonPath: String, value: String) {
-        val json = """"
-            {"key1": {"key2": "test"}}
-            """".trimMargin()
-        invoking { json.assertByJsonPath(value, jsonPath) } `should not throw` AssertionError::class
+    @Step("<jsonPath>の値を取得した結果が文字列の<value>である")
+    fun getJsonPathAsString(jsonPath: String, value: String) {
+        // JsonNodeから値を取得した結果、なんかしらのライブラリでアサーションする
+        invoking { TODO() } `should not throw` AssertionError::class
     }
 
-    @Step("<jsonPath>が真偽値の<value>であることをアサー卜できる")
-    fun assertJsonPathBoolean(jsonPath: String, value: Boolean) {
-        val json = """"
-            {"key1": {"key2": $value}}
-            """".trimMargin()
-        invoking { json.assertByJsonPath(value, jsonPath) } `should not throw` AssertionError::class
+    @Step("<jsonPath>の値を取得した結果が真偽値の<value>である")
+    fun getJsonPathAsBoolean(jsonPath: String, value: String) {
+        // JsonNodeから値を取得した結果、なんかしらのライブラリでアサーションする
+        invoking { TODO() } `should not throw` AssertionError::class
     }
 
-    @Step("<jsonPath>が整数値の<value>であることをアサー卜できる")
-    fun assertJsonPathInt(jsonPath: String, value: Int) {
-        val json = """"
-            {"key1": {"key2": 1}}
-            """".trimMargin()
-        invoking { json.assertByJsonPath(value, jsonPath) } `should not throw` AssertionError::class
+    @Step("<jsonPath>の値を取得した結果が整数の<value>である")
+    fun getJsonPathAsInteger(jsonPath: String, value: String) {
+        invoking { TODO() } `should not throw` AssertionError::class
     }
 
-    @Step("<jsonPath>が小数値の<value>であることをアサー卜できる")
-    fun assertJsonPathDouble(jsonPath: String, value: Double) {
-        val json = """"
-            {"key1": {"key2": 1.0}}
-            """".trimMargin()
-        invoking { json.assertByJsonPath(value, jsonPath) } `should not throw` AssertionError::class
+    @Step("<jsonPath>の値を取得した結果が小数の<value>である")
+    fun getJsonPathAsFloat(jsonPath: String, value: String) {
+        invoking { TODO() } `should not throw` AssertionError::class
     }
 }
+
+
