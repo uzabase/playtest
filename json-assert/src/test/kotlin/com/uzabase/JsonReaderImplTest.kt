349e8b434ac0c46813a8f6b1b6211a7a7e04c27b
jnuank <ikuta1919@gmail.com>
2021-10-27 18:52:24 +0900

    JsonNodeクラスでの値取得を実装

--- json-assert/src/test/kotlin/com/uzabase/JsonReaderImplTest.kt
+++ json-assert/src/test/kotlin/com/uzabase/JsonReaderImplTest.kt
@@ -6,34 +6,55 @@ import org.amshove.kluent.shouldThrow
 import org.junit.jupiter.api.Test
 
 internal class JsonReaderImplTest {
-    val jsonReader = JsonReaderImpl()
-
     @Test
     fun JSONPathで指定したKeyの値が存在しないとき例外をスローする() {
-        invoking { jsonReader.getValueByJsonPath<String>("""{"test": "test"}""", "$.xxx") } shouldThrow NotFoundJsonValueException::class
+        invoking {
+            val jsonString = """
+                {"key1": "value1"}
+            """.trimIndent()
+
+            val jsonNode = JsonNode.of(jsonString)
+            jsonNode.get("$.key999")
+        } shouldThrow NotFoundJsonValueException::class
     }
 
     @Test
     fun JSONPathで指定したKeyの文字列を取得する() {
-        val actual = jsonReader.getStringByJsonPath("""{"test": "test"}""", "$.test")
-        "test" shouldBeEqualTo actual
+        val jsonString = """
+            {"key1": "value1"}
+        """.trimIndent()
+
+        val jsonNode = JsonNode.of(jsonString)
+        "value1" shouldBeEqualTo jsonNode.get<String>("$.key1")
     }
 
     @Test
     fun JSONPathで指定したKeyの真偽値を取得する() {
-        val actual = jsonReader.getBooleanByJsonPath("""{"test": true}""", "$.test")
-        true shouldBeEqualTo actual
+        val jsonString = """
+            {"key1": true }
+        """.trimIndent()
+
+        val jsonNode = JsonNode.of(jsonString)
+        true shouldBeEqualTo jsonNode.get<Boolean>("$.key1")
     }
 
     @Test
     fun JSONPathで指定したKeyの整数値を取得する() {
-        val actual = jsonReader.getIntByJsonPath("""{"test": 1}""", "$.test")
-        1 shouldBeEqualTo actual
+        val jsonString = """
+            {"key1": 1}
+        """.trimIndent()
+
+        val jsonNode = JsonNode.of(jsonString)
+        1 shouldBeEqualTo jsonNode.get<Int>("$.key1")
     }
 
     @Test
     fun JSONPathで指定したKeyの小数値を取得する() {
-        val actual = jsonReader.getDoubleByJsonPath("""{"test": 1.0}""", "$.test")
-        1.0 shouldBeEqualTo actual
+        val jsonString = """
+            {"key1": 1.2}
+        """.trimIndent()
+
+        val jsonNode = JsonNode.of(jsonString)
+        1.2 shouldBeEqualTo jsonNode.get<Double>("$.key1")
     }
 }
