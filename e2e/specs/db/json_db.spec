372c0fc0cc2f536de4ca3c3ddcade65a460e03d7
YOSHIKI <gpdt-tokyo1@uzabase.com>
2021-11-18 19:59:51 +0900

    e2eの表現に「一意」を追加した&数値のシナリオを追加した

--- e2e/specs/db/json_db.spec
+++ e2e/specs/db/json_db.spec
@@ -2,5 +2,8 @@
 
 ## クエリを指定して、一意の値(文字列)を取得する
 tags: unimplemented
-* "schemaName"スキーマの"tableName"テーブルの、"columnName"を"whereString"で取得した結果が文字列の"test"である
+* "schemaName"スキーマの"tableName"テーブルの、"columnName"を"whereString"で取得した一意の結果が文字列の"test"である
 
+## クエリを指定して、一意の値(整数)を取得する
+tags: unimplemented
+* "schemaName"スキーマの"tableName"テーブルの、"columnName"を"whereString"で取得した一意の結果が整数の"test"である
--- e2e/src/test/kotlin/com/uzabase/Database.kt
+++ e2e/src/test/kotlin/com/uzabase/Database.kt
@@ -4,12 +4,21 @@ import com.thoughtworks.gauge.Step
 import org.amshove.kluent.shouldBeEqualTo
 
 class Database {
-    @Step("<schemaName>スキーマの<tableName>テーブルの、<columnName>を<whereString>で取得した結果が文字列の<vale>である")
-            fun getParameterFromDatabase(schemaName: String, tableName: String, columnName: String, whereString: String, value: String) {
+    @Step("<schemaName>スキーマの<tableName>テーブルの、<columnName>を<whereString>で取得した一意の結果が文字列の<vale>である")
+            fun getStringParameterFromDatabase(schemaName: String, tableName: String, columnName: String, whereString: String, value: String) {
                 val query = """
                     select $columnName from $schemaName.$tableName where $whereString;
                 """.trimIndent()
                val result = TODO() //いい感じにSQLを実行する
                value shouldBeEqualTo result
             }
+
+    @Step("<schemaName>スキーマの<tableName>テーブルの、<columnName>を<whereString>で取得した一意の結果が整数の<vale>である")
+    fun getIntParameterFromDatabase(schemaName: String, tableName: String, columnName: String, whereString: String, value: Int) {
+        val query = """
+                    select $columnName from $schemaName.$tableName where $whereString;
+                """.trimIndent()
+        val result = TODO() //いい感じにSQLを実行する
+        value shouldBeEqualTo result
+    }
 }
