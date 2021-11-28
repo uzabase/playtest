39bdb7fdb0387f3c6528698c6c377df48cbeb218
yoshiaki.harada <yoshiaki.harada@uzabase.com>
2021-11-28 18:22:30 +0900

    条件文で値を取得するステップをついか

--- playtest-gauge-db/src/main/kotlin/com/uzabase/playtest/gauge/db/DatabaseStep.kt
+++ playtest-gauge-db/src/main/kotlin/com/uzabase/playtest/gauge/db/DatabaseStep.kt
@@ -3,6 +3,7 @@ package com.uzabase.playtest.gauge.db
 import com.thoughtworks.gauge.Step
 import org.assertj.db.api.Assertions.assertThat
 import org.assertj.db.type.*
+import java.sql.Date
 
 class DatabaseStep {
 
@@ -28,6 +29,27 @@ class DatabaseStep {
         assertThat(request).row().value(valueColumn).isEqualTo(value)
     }
 
+    @Step(
+        "DB<dbName>の<schemaName>スキーマの<tableName>テーブルの、条件<where>で取得した一意の<valueColumn>が整数の<value>である",
+        "DB<dbName>の<schemaName>スキーマの<tableName>テーブルの、条件<where>で取得した一意の<valueColumn>が小数の<value>である",
+        "DB<dbName>の<schemaName>スキーマの<tableName>テーブルの、条件<where>で取得した一意の<valueColumn>が文字列の<value>である"
+    )
+    fun assertUniqueRecordValue(
+        dbName: String,
+        schemaName: String,
+        tableName: String,
+        where: String,
+        valueColumn: String,
+        value: String
+    ) {
+        val config = GaugeDbConfig.get(dbName)
+        val source = Source(config.url, config.user, config.password)
+
+        val request = Request(source, "select * from $schemaName.$tableName where $where")
+
+        assertThat(request).row().value(valueColumn).isEqualTo(value)
+    }
+
     @Step("DB<dbName>の<schemaName>スキーマの<tableName>テーブルの、<whereColumn>を<whereValue>で取得した一意の<valueColumn>が<year>年<month>月<day>日である")
     fun assertUniqueRecordDateValue(
         dbName: String,
@@ -46,6 +68,23 @@ class DatabaseStep {
         val request = Request(source, "select * from $schemaName.$tableName where $whereColumn = '$whereValue'")
 
         assertThat(request).row().value(valueColumn).isEqualTo(DateValue.of(year, month, day))
+    }
+
+    @Step("DB<dbName>の<schemaName>スキーマの<tableName>テーブルの、条件<where>で取得した一意の<valueColumn>が日付の<date>である")
+    fun assertUniqueRecordDateValue(
+        dbName: String,
+        schemaName: String,
+        tableName: String,
+        where: String,
+        valueColumn: String,
+        date: String
+    ) {
+        val config = GaugeDbConfig.get(dbName)
+        val source = Source(config.url, config.user, config.password)
+
+        val request = Request(source, "select * from $schemaName.$tableName where $where")
+
+        assertThat(request).row().value(valueColumn).isEqualTo(Date.valueOf(date))
     }
 
     @Step("DB<dbName>の<schemaName>スキーマの<tableName>テーブルの、<whereColumn>を<whereValue>で取得した一意の<valueColumn>が<year>年<month>月<day>日<hours>時<minutes>分<seconds>秒である")
