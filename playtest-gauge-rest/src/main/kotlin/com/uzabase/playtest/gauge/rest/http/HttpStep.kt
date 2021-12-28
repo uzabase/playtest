4822aed2aa11a809af50d54f7a0037cc5e4ff3b8
Yoshiaki Harada <yoshiaki.harada@uzabase.com>
2021-12-28 16:51:49 +0900

    urlの連結をURLオブジェクトでやるようにした

--- playtest-gauge-rest/src/main/kotlin/com/uzabase/playtest/gauge/rest/http/HttpStep.kt
+++ playtest-gauge-rest/src/main/kotlin/com/uzabase/playtest/gauge/rest/http/HttpStep.kt
@@ -5,95 +5,97 @@ import com.uzabase.playtest.gauge.rest.ConfigKeys
 import com.uzabase.playtest.gauge.rest.DataStore
 import com.uzabase.playtest.gauge.rest.GaugeRestConfig
 import org.junit.jupiter.api.Assertions
+import java.net.URL
 
 class HttpStep {
     @Step("URL<url>にGETリクエストを送る")
     fun executeGet(url: String) {
-        val endpoint = getBaseUrl() + url
-        val (statusCode, headers, body) = HttpClient().executeGet(endpoint)
+        val endpoint = generateEndpoint(url)
+        val (statusCode, headers, body) = HttpClient().executeGet(endpoint.toString())
         DataStore.storeResponseData(statusCode, headers, body)
     }
 
+
     @Step("URL<url>にヘッダー<header>で、GETリクエストを送る")
     fun executeGet(url: String, header: String) {
-        val endpoint = getBaseUrl() + url
-        val (statusCode, headers, body) = HttpClient().executeGet(endpoint, toHeaderMap(header))
+        val endpoint = generateEndpoint(url)
+        val (statusCode, headers, body) = HttpClient().executeGet(endpoint.toString(), toHeaderMap(header))
         DataStore.storeResponseData(statusCode, headers, body)
     }
 
     @Step("URL<url>にPUTリクエストを送る")
     fun executePut(url: String) {
-        val endpoint = getBaseUrl() + url
-        val (statusCode, headers, body) = HttpClient().executePut(endpoint)
+        val endpoint = generateEndpoint(url)
+        val (statusCode, headers, body) = HttpClient().executePut(endpoint.toString())
         DataStore.storeResponseData(statusCode, headers, body)
     }
 
     @Step("URL<url>にヘッダー<header>で、PUTリクエストを送る")
     fun executePutWithHeader(url: String, header: String) {
-        val endpoint = getBaseUrl() + url
-        val (statusCode, headers, body) = HttpClient().executePut(endpoint, toHeaderMap(header))
+        val endpoint = generateEndpoint(url)
+        val (statusCode, headers, body) = HttpClient().executePut(endpoint.toString(), toHeaderMap(header))
         DataStore.storeResponseData(statusCode, headers, body)
     }
 
     @Step("URL<url>にボディ<requestBody>、ヘッダー<header>で、PUTリクエストを送る")
     fun executePut(url: String, requestBody: String, header: String) {
-        val endpoint = getBaseUrl() + url
-        val (statusCode, headers, body) = HttpClient().executePut(endpoint, requestBody, toHeaderMap(header))
+        val endpoint = generateEndpoint(url)
+        val (statusCode, headers, body) = HttpClient().executePut(endpoint.toString(), requestBody, toHeaderMap(header))
         DataStore.storeResponseData(statusCode, headers, body)
     }
 
     @Step("URL<url>にボディ<requestBody>で、PUTリクエストを送る")
     fun executePut(url: String, requestBody: String) {
-        val endpoint = getBaseUrl() + url
+        val endpoint = generateEndpoint(url)
         val (statusCode, headers, body) = HttpClient().executePut(
-            endpoint,
-            requestBody
+                endpoint.toString(),
+                requestBody
         )
         DataStore.storeResponseData(statusCode, headers, body)
     }
 
     @Step("URL<url>にPOSTリクエストを送る")
     fun executePost(url: String) {
-        val endpoint = getBaseUrl() + url
-        val (statusCode, headers, body) = HttpClient().executePost(endpoint)
+        val endpoint = generateEndpoint(url)
+        val (statusCode, headers, body) = HttpClient().executePost(endpoint.toString())
         DataStore.storeResponseData(statusCode, headers, body)
     }
 
     @Step("URL<url>にヘッダー<header>で、POSTリクエストを送る")
     fun executePostWithHeader(url: String, header: String) {
-        val endpoint = getBaseUrl() + url
-        val (statusCode, headers, body) = HttpClient().executePost(endpoint, toHeaderMap(header))
+        val endpoint = generateEndpoint(url)
+        val (statusCode, headers, body) = HttpClient().executePost(endpoint.toString(), toHeaderMap(header))
         DataStore.storeResponseData(statusCode, headers, body)
     }
 
     @Step("URL<url>にボディ<requestBody>、ヘッダー<header>で、POSTリクエストを送る")
     fun executePost(url: String, requestBody: String, header: String) {
-        val endpoint = getBaseUrl() + url
-        val (statusCode, headers, body) = HttpClient().executePost(endpoint, requestBody, toHeaderMap(header))
+        val endpoint = generateEndpoint(url)
+        val (statusCode, headers, body) = HttpClient().executePost(endpoint.toString(), requestBody, toHeaderMap(header))
         DataStore.storeResponseData(statusCode, headers, body)
     }
 
     @Step("URL<url>にボディ<requestBody>で、POSTリクエストを送る")
     fun executePost(url: String, requestBody: String) {
-        val endpoint = getBaseUrl() + url
+        val endpoint = generateEndpoint(url)
         val (statusCode, headers, body) = HttpClient().executePost(
-            endpoint,
-            requestBody
+                endpoint.toString(),
+                requestBody
         )
         DataStore.storeResponseData(statusCode, headers, body)
     }
 
     @Step("URL<url>にDELETEリクエストを送る")
     fun executeDelete(url: String) {
-        val endpoint = getBaseUrl() + url
-        val (statusCode, headers, body) = HttpClient().executeDelete(endpoint)
+        val endpoint = generateEndpoint(url)
+        val (statusCode, headers, body) = HttpClient().executeDelete(endpoint.toString())
         DataStore.storeResponseData(statusCode, headers, body)
     }
 
     @Step("URL<url>にヘッダー<header>で、DELETEリクエストを送る")
     fun executeDelete(url: String, header: String) {
-        val endpoint = getBaseUrl() + url
-        val (statusCode, headers, body) = HttpClient().executeDelete(endpoint, toHeaderMap(header))
+        val endpoint = generateEndpoint(url)
+        val (statusCode, headers, body) = HttpClient().executeDelete(endpoint.toString(), toHeaderMap(header))
         DataStore.storeResponseData(statusCode, headers, body)
     }
 
@@ -111,13 +113,15 @@ class HttpStep {
 
     fun toHeaderMap(header: String): Map<String, String> {
         return header
-            .split("r\n")
-            .associate {
-                it.split(":")
-                    .map { v -> v.trim() }
-                    .let { h -> h[0] to h[1] }
-            }
+                .split("r\n")
+                .associate {
+                    it.split(":")
+                            .map { v -> v.trim() }
+                            .let { h -> h[0] to h[1] }
+                }
     }
+
+    private fun generateEndpoint(url: String) = URL(URL(getBaseUrl()), url)
 
     private fun getBaseUrl(): String {
         return GaugeRestConfig.get(ConfigKeys.BASE_URL)
