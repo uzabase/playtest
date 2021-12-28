32935ca0d1547738eb6388d4d428878f8f9eb64c
Yoshiaki Harada <yoshiaki.harada@uzabase.com>
2021-12-28 16:00:19 +0900

    webdriver mangerを入れた

--- playtest-gauge-browser/src/test/kotlin/com/uzabase/playtest/gauge/browser/SetupAndTeardown.kt
+++ playtest-gauge-browser/src/test/kotlin/com/uzabase/playtest/gauge/browser/SetupAndTeardown.kt
@@ -3,11 +3,13 @@ package com.uzabase.playtest.gauge.browser
 import com.codeborne.selenide.Selenide
 import com.thoughtworks.gauge.BeforeScenario
 import com.thoughtworks.gauge.BeforeSuite
+import io.github.bonigarcia.wdm.WebDriverManager
 
 class SetupAndTeardown {
     @BeforeSuite()
     fun setup() {
         System.setProperty("selenide.baseUrl", "http://localhost:${TestConfig.getInt(TestConfig.wiremockPort)}")
+        WebDriverManager.chromedriver().setup();
     }
 
     @BeforeScenario()
