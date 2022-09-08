c95be3a703e17502a0a067a2912e06fd5fe096d3
nilwurtz <apollo.oth@gmail.com>
2022-09-08 21:56:47 +0900

    :bug: playtest-gauge-db: fix `GaugeDbConfig.getRecords()`

--- playtest-gauge-db/src/test/kotlin/com/uzabase/playtest/gauge/db/DatabaseChangesTest.kt
+++ playtest-gauge-db/src/test/kotlin/com/uzabase/playtest/gauge/db/DatabaseChangesTest.kt
@@ -1,14 +1,11 @@
 package com.uzabase.playtest.gauge.db
 
-import io.mockk.*
 import org.amshove.kluent.`should be equal to`
-import org.assertj.db.type.Changes
-import org.assertj.db.type.Source
 import org.junit.jupiter.api.Test
 
 internal class DatabaseChangesTest {
     @Test
-    fun DatabseChangesでセットアップする() {
+    fun DatabaseChangesでセットアップする() {
         val databaseChanges = DatabaseChanges("test")
         databaseChanges.setup()
         databaseChanges.isStarted() `should be equal to` false
