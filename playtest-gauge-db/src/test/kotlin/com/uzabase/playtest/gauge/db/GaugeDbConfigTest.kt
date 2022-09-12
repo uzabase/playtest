3be758e431fbb387a93e10cfa82b3a8366ebb35b
haradayoshiaki <y.team.kobe@gmail.com>
2022-09-12 21:40:59 +0900

    playtest-gauge-browser: 記録対象取得のテストを修正

--- playtest-gauge-db/src/test/kotlin/com/uzabase/playtest/gauge/db/GaugeDbConfigTest.kt
+++ playtest-gauge-db/src/test/kotlin/com/uzabase/playtest/gauge/db/GaugeDbConfigTest.kt
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test
 internal class GaugeDbConfigTest {
     @Test
     fun ConfigからDB名をリストで取得する() {
+        System.setProperty("db.z_test.url", "jdbc:postgresql://localhost:5433/z_test")
         val actual = GaugeDbConfig.getRecords()
 
         actual shouldBeEqualTo listOf("test_db", "z_test")
