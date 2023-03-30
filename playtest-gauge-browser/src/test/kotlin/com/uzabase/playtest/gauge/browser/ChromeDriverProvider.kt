10ed8e99b3efec29a2a9f96f17bb4086a411631f
ayato-p <ayato.nishimura@uzabase.com>
2023-03-30 23:51:29 +0900

    Tweak

--- playtest-gauge-browser/src/test/kotlin/com/uzabase/playtest/gauge/browser/ChromeDriverProvider.kt
+++ playtest-gauge-browser/src/test/kotlin/com/uzabase/playtest/gauge/browser/ChromeDriverProvider.kt
@@ -1,13 +1,12 @@
 package com.uzabase.playtest.gauge.browser
 
 import com.codeborne.selenide.WebDriverProvider
-import io.github.bonigarcia.wdm.WebDriverManager
 import org.openqa.selenium.Capabilities
 import org.openqa.selenium.WebDriver
 import org.openqa.selenium.chrome.ChromeDriver
 import org.openqa.selenium.chrome.ChromeOptions
 
-class ChromeDriverProvider: WebDriverProvider {
+class ChromeDriverProvider : WebDriverProvider {
     override fun createDriver(p0: Capabilities): WebDriver {
         val options = ChromeOptions()
         // for Chrome 111 issue
