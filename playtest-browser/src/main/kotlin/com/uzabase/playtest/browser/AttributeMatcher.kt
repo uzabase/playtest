b50219c5eefd2a6dad3a370a60df21fc017ab210
ItsukiYamada <yamada351035@gmail.com>
2024-06-24 21:10:50 +0900

    :bug:fix:

--- playtest-browser/src/main/kotlin/com/uzabase/playtest/browser/AttributeMatcher.kt
+++ playtest-browser/src/main/kotlin/com/uzabase/playtest/browser/AttributeMatcher.kt
@@ -1,26 +1,28 @@
 package com.uzabase.playtest.browser
 
 import com.codeborne.selenide.CheckResult
-import com.codeborne.selenide.Condition
 import com.codeborne.selenide.Driver
+import com.codeborne.selenide.WebElementCondition
 import org.openqa.selenium.WebElement
 
-fun attributeStartsWith(key: String, value: String): Condition {
-    return object : Condition("attributeStartsWith") {
-        override fun apply(driver: Driver, element: WebElement): Boolean =
-            element.getAttribute(key).startsWith(value)
+
+fun attributeStartsWith(key: String, value: String): WebElementCondition {
+    return object : WebElementCondition("attributeStartsWith") {
+        override fun check(driver: Driver, element: WebElement): CheckResult =
+            CheckResult(element.getAttribute(key).startsWith(value), element.getAttribute(key))
     }
 }
 
-fun attributeEndsWith(key: String, value: String): Condition {
-    return object : Condition("attributeEndsWith") {
-        override fun apply(driver: Driver, element: WebElement): Boolean =
-            element.getAttribute(key).endsWith(value)
+fun attributeEndsWith(key: String, value: String): WebElementCondition {
+    return object : WebElementCondition("attributeEndsWith") {
+        override fun check(driver: Driver, element: WebElement): CheckResult =
+            CheckResult(element.getAttribute(key).endsWith(value), element.getAttribute(key))
     }
 }
 
-fun attributeContains(key: String, value: String): Condition {
-    return object : Condition("attributeContains") {
-        override fun apply(driver: Driver, element: WebElement): Boolean = element.getAttribute(key).contains(value)
+fun attributeContains(key: String, value: String): WebElementCondition {
+    return object : WebElementCondition("attributeContains") {
+        override fun check(driver: Driver, element: WebElement): CheckResult =
+            CheckResult(element.getAttribute(key).contains(value), element.getAttribute(key))
     }
 }
--- playtest-browser/src/main/kotlin/com/uzabase/playtest/browser/CssMatcher.kt
+++ playtest-browser/src/main/kotlin/com/uzabase/playtest/browser/CssMatcher.kt
@@ -1,14 +1,13 @@
 package com.uzabase.playtest.browser
 
-import com.codeborne.selenide.Condition
+import com.codeborne.selenide.CheckResult
 import com.codeborne.selenide.Driver
+import com.codeborne.selenide.WebElementCondition
 import org.openqa.selenium.WebElement
 
-fun css(propName: String, propValue: String): Condition {
-    return object : Condition("css") {
-        override fun apply(driver: Driver, element: WebElement): Boolean =
-            propValue.equals(element.getCssValue(propName), ignoreCase = true)
-
-        override fun actualValue(driver: Driver, element: WebElement): String? = element.getCssValue(propName)
+fun css(propName: String, propValue: String): WebElementCondition {
+    return object : WebElementCondition("css") {
+        override fun check(driver: Driver, element: WebElement): CheckResult =
+            CheckResult(propValue.equals(element.getCssValue(propName), ignoreCase = true), element.getCssValue(propName))
     }
 }
